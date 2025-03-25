6af851b3a920b6c6438d9b362a118d1c5fa7eac0
ForzaETH race crew <group_2767_bot_42d88307b2ed7f1b43e3ae4e11796a6e:n5kVBi3i7qZuWbestsWy@noreply.example.com>
2025-03-25 14:35:18 +0100

    fix path

--- start_vnc.sh
+++ start_vnc.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 export DISPLAY=:123
-exec supervisord -c  "$HOME/catkin_ws/src/race_stack/.devcontainer/.vnc_utils/supervisord.conf"
+exec supervisord -c  "$VNC_DIR/supervisord.conf"
