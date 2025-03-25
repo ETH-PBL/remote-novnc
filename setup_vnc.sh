440cd9b3f9c72b4bdbb41b83e4e2c500b876cfb0
Edoardo Ghignone <eghignone@pbl.ee.ethz.ch>
2025-03-25 12:49:06 +0100

    some modifications

--- setup_vnc.sh
+++ setup_vnc.sh
@@ -2,6 +2,8 @@
 
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install supervisor novnc x11vnc xvfb xfce4 xfce4-goodies -y
-echo 'export DISPLAY=:1' >> ~/.bashrc
-echo 'export VNC_DIR="$HOME/catkin_ws/src/race_stack/.devcontainer/.vnc_utils"' >> ~/.bashrc
+
+# use current folder 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "export VNC_DIR=\"$SCRIPT_DIR\"" >> ~/.bashrc
 
--- start_vnc.sh
+++ start_vnc.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
+export DISPLAY=:123
 exec supervisord -c  "$HOME/catkin_ws/src/race_stack/.devcontainer/.vnc_utils/supervisord.conf"
